9b349630538ff2605a204b0ff43f617566c7fa93
Fedor <andryushin.fedor@gmail.com>
2020-02-21 11:00:02 +0300

    s

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let animation01 = anime({
+    targets: '.square',
+    autoplay: true,
+    duration: 4000,
+    direction: 'alternate',
+    translateX: -1920,
+    loop: true,
+    easing: 'easeInOutSine',
+  })
+
+  let animation02 = anime({
+    targets: '.rectangle1',
+    autoplay: true,
+    duration: 6000,
+    direction: 'alternate',
+    translateX: 1920,
+    loop: true,
+    easing: 'easeInOutSine',
+  })
+  
+  let animation03 = anime({
+    targets: '.rectangle2',
+    autoplay: true,
+    duration: 4000,
+    direction: 'alternate',
+    translateX: -1920,
+    loop: true,
+    easing: 'easeInOutSine',
+  })
